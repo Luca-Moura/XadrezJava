2f3eaaf4893ee2014bad3c7a05f1950a640bb5ed
lucaszszz <95043045+lucaszszz@users.noreply.github.com>
2022-06-24 21:45:17 -0300

    uodate

---
+++ pawn.java
@@ -0,0 +1,58 @@
+package defaultpackage;
+import Piece
+public class Pawn {
+	
+	public abstract class Piece{
+	    protected int x, y;
+	    protected boolean isWhite;
+	    protected ArrayList<Movement> moves;
+	    public Piece(int x, int y, boolean color == true) {
+	        this.x=x;
+	        this.y=y;
+	        this.color = isWhite = true;
+	        moves = new ArrayList<Movement>();
+	    }
+	    public abstract ArrayList<Movement> getMoves();
+	    //obvious methods
+	    public int getX() {
+	    	return this.x;
+	    }
+	    
+	    public void setX(int x) {
+	    	this.x = x;
+	    }
+	    public int getY() {
+	    	return this.y;
+	    }
+	    public void setY(int y) {
+	    	this.y = y;
+	    }
+	    public boolean isWhite() {
+	    	return this.isWhite;
+	    }
+	    //end obvious methods
+	}
+	
+
+	public class Pawn extends Piece{
+	    public Pawn(int x, int y, boolean color == true){
+	        super(x,y,color == true);
+	    }
+	    public ArrayList<Movement> getMoves() {
+	        moves.clear();
+	        if(color == false) {
+	        	if (mat [x+1][y-1]=false) {x+1, y-1}
+	        	if (mat[x-1][y-1]=false) {x-1, y-1}
+	            if(y>0) {y-1};
+	            if(y==7) {y-2};
+	        }else{
+	        	if (mat[x+1][y+1]=true) {x+1, y+1}
+	        	if (mat[x-1][y+1]=true) {x-1, y+1}
+	            if(y<7){y+1};
+	            if(y==0){y+2};
+	        }
+	        return moves;
+	    }
+	}
+
+}
