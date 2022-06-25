9aea3d260760c905cfd730c896959c9b3345ec53
Hugo matos <103541536+senamatos@users.noreply.github.com>
2022-06-24 21:43:57 -0300

    Add files via upload

---
+++ Knight.java
@@ -0,0 +1,24 @@
+package Object;
+import object.Board;
+import object.Movement;
+import object.Piece
+
+public class Knight extends Piece {
+
+
+//To calculate possible moves
+static int findPossibleMoves(int mat[][])
+{
+    // All possible moves of a knight
+    int x[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+    int y[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
+    int count = 0;
+
+    // Check if each possible move is valid or not
+    for (int i = 0; i < 8; i++) {
+
+    }
+}
+}
+
